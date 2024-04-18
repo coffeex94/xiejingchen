3ebe363ad840da33b815d481408d35a4f5e3895c
coffeex94 <150662254+coffeex94@users.noreply.github.com>
2024-04-18 21:32:36 +0800

    Add files via upload

---
+++ baidujsSdk@2.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: hAo
+ * @LastEditors: hAo
+ * @Date: 2020-03-27 18:46:16
+ * @LastEditTime: 2020-03-27 18:46:22
+ */
+(function () {
+    window.HOST_TYPE = "2";
+    window.BMap_loadScriptTime = (new Date).getTime();
+    document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=2.0&ak=ZxQ1WQz82jvbZPlrPr0KIRaATq6CEQU0&services=&t=20200327103013"></script>');
+})();
